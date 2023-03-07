92cde1e6b75e041fab51c49b22b5c116fd4ef3ea
nick <mwauranicholas10@gmail.com>
2023-03-07 23:56:02 +0300

    homecard.js

---
+++ src/components/HomeCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+export default function ProjectCard({title,description,id, deleteProject}) {
+
+
+
+
+  return (
+    <div key={id}>
+
+        <div key={id} className="projects-card">
+                <p className="project-title">{title}</p>
+                <p>{description}</p>
+                <button className='delete-btn' onClick={()=> deleteProject(id)}>delete<ion-icon name="trash-outline"></ion-icon></button>                
+        </div> 
+      
+    </div>
+  )
+}
+
