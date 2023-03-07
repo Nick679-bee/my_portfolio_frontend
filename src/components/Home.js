cf46f4dd9ed5befffda296e4c0693c45d3681c68
nick <mwauranicholas10@gmail.com>
2023-03-07 23:38:44 +0300

    added home.js

---
+++ src/components/Home.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import './Home.css'
+import HomeCard from './HomeCard'
+
+function Home() {
+
+  const [profile, setProfile] = useState([])
+
+
+  useEffect( ()=>{
+      fetch()
+      .then(res => res.json())
+      .then(data =>{
+          console.log(data)
+          setProfile(data)
+      })
+  },[])
+
+
+  function deleteProject(id){
+      fetch(`${id}`,{
+        method: "DELETE"
+      })
+      .then(()=>{
+          setProfile((prof) => prof.filter((it) => it.id !== id))
+      })
+  }
+
+
+
+
+  return (
+    <div className='background'>
+            <h2 className='title-h2'>WELCOME TO MY-PORTFOLIO</h2>
+
+      <div className='home'>
+        <div className='card-avatar'>
+            
+            <div class="container">
+                <h4><b>John Doe</b></h4>
+                <p>Software Engineer</p>
+            </div>
+        </div>
+        <div className="registration">
+            <div className="r-container">
+                    <div className="r-left">
+                    <p className="r-title">Check out what others are working on to get inspiration to be innovative...</p>
+
+                 </div>
+            </div>
+
+            <div className="r2-container">
+                <div className="r-right"></div>
+                <div className="flex-container">
+
+                    {profile.map((value)=><HomeCard title={value.title} description={value.description} key={value.id} id={value.id} deleteProject={deleteProject}/>)}
+
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
